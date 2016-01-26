db70a37b993fef6cebd43564fd0af3a09f9ccc7b
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-01-26 12:13:19 -0600

    added cal test

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const { expect } = require('chai');
+const cp = require('child_process');
 
-describe('test suite', () => {
-	it('true should be true', () => {
-		expect(true).to.be.true;
+describe('calenar application', () => {
+	it('should be able to handle the current month', () => {
+		// compare our cal app with the built in calendar app
+		// execSync is a method on child process;
+		const goal = cp.execSync('cal').toString();
+		const output = cp.execSync('./cal.js').toString();
+
+
+		expect(output).to.equal(goal);
 	});
 });
